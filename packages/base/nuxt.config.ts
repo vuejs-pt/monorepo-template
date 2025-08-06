d9e9677c0bcffc092f62b78fb474774d9f578ccb
Rui Posse <ruiposse@gmail.com>
2025-08-06 17:02:06 +0200

    chore: tailwind on base package

--- packages/base/nuxt.config.ts
+++ packages/base/nuxt.config.ts
@@ -1,5 +1,13 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })

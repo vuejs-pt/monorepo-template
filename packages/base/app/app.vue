3fd64850226340fd19d0b9b868d79b424e7abed2
LuisMota93 <luisffsmota@hotmail.com>
2025-09-30 12:04:13 +0100

    feat: set about us page in all layers.

--- packages/base/app/app.vue
+++ packages/base/app/app.vue
@@ -1,25 +1,3 @@
 <template>
-  <div class="container mx-auto py-8">
-    <h1 class="text-5xl font-bold text-center mb-10">{{ config.public.name }}</h1>
-    <h2 class="text-3xl font-bold text-center underline mb-8">
-      Hello world!
-    </h2>
-    <div class="flex justify-center p-4 mb-8">
-      <Button variant="default">Just a button</Button>
-    </div>
-    <div class="flex justify-center p-4 mb-8">
-      <Button :variant="variant" @click="toggleVariant">Apply other variant dynamically</Button>
-    </div>
-  </div>
+  <NuxtPage />
 </template>
-
-<script setup lang="ts">
-import { Button } from 'ui'
-
-const config = useRuntimeConfig()
-
-const variant = ref('custom')
-const toggleVariant = () => {
-  variant.value = variant.value === 'custom' ? 'default' : 'custom'
-}
-</script>
---
+++ tenants/clientB/app/pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NuxtLink to="/" class="flex items-center">
+      <ArrowLeft :size="18" />
+      Back to the homepage
+    </NuxtLink>
+    <h1 class="mb-10 text-center">About us ClientB</h1>
+    <div class="flex justify-center mb-8">
+      <Button variant="default">Default Variant</Button>
+    </div>
+    <div class="flex justify-center mb-8">
+      <Button variant="custom">Custom Variant</Button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ArrowLeft } from 'lucide-vue-next'
+import { Button } from 'ui'
+</script>
